ad4c29a0f4aae4eb38848edea1fc434f963b7a30
YashThenginkaiSunil <yashteng@gmail.com>
2025-06-29 20:14:23 +0530

    user login done

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -2,7 +2,7 @@ const userModel = require('../models/user.model.js');
 const userService = require('../services/user.service.js');
 const { validationResult } = require('express-validator');
 
-module.exports.registerUser = async (req, res, next) => 
+module.exports.registerUser = async (req, res) => 
 {
     const errors = validationResult(req);
     if (!errors.isEmpty())
@@ -11,7 +11,6 @@ module.exports.registerUser = async (req, res, next) =>
     }
 
     const { fullname, email, password } = req.body;
-
     const hashedPassword = await userModel.hashPassword(password);
 
     const user = await userService.createUser(
@@ -24,8 +23,39 @@ module.exports.registerUser = async (req, res, next) =>
     );
 
     const token = user.generateAuthToken();
-
     res.status(201).json({ token, user });
-
-
 }
+
+
+module.exports.loginUser = async (req, res) => 
+{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) 
+    {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email }).select('+password');// '+' --> overrides 'select:false' which now includes the password field  
+
+    if (!user) // if user not present
+    {
+        return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const didPasswordMatch = await user.comparePassword(password); // compare pwd
+
+    if (!didPasswordMatch) 
+    {
+        return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const token = user.generateAuthToken();
+    res.status(200).json({ token, user });
+}
+
+
+
+
+
